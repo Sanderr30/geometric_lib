11e5113c01dbf5a2587285f54f777db42f4df54c
Alexander <451249@niuitmo.ru>
2024-11-13 16:44:12 +0300

    fourth try

--- square.py
+++ square.py
@@ -3,6 +3,7 @@ def area(a):
         raise ValueError("Value of square sides can not be below 0")
     return a * a
 
+
 def perimeter(a):
     if a < 0:
         raise ValueError("Value of square sides can not be below 0")
